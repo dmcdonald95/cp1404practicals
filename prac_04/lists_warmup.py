b4d17873904afc50345d07d616bb43d8bd767f34
DMcDonald <david.mcdonald1@my.jcu.edu.au>
2020-08-25 12:59:28 +1000

    Add lists warmup file

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,17 @@
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+# print(numbers[0])  # will print first number in list (3)
+# print(numbers[-1])  # will print last number in list (2)
+# print(numbers[3])  # will print fourth number in list (1)
+# print(numbers[:-1])  # will print all numbers except last in list
+# print(numbers[3:4])  # will slice out first three numbers and what ever remains after the fourth number
+# print(5 in numbers)  # will print 5 if present in the list / will say True or False if present
+# print(7 in numbers)  # will print 7 if present in list / will say True or False if present
+# print("3" in numbers)  # will print "3" if present in list / will say True of False if present
+# print(numbers + [6, 5, 3])  # will print original list of numbers with the addition of 6, 5, and 3
+
+numbers[0] = "ten"
+numbers[-1] = 1
+print(numbers)
+print(numbers[2:])
+print(9 in numbers)

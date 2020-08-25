b081f7f8f019be5029cc1a081aa0d2c3ec95467f
DMcDonald <david.mcdonald1@my.jcu.edu.au>
2020-08-25 12:26:00 +1000

    Refactor broken score program to use functions

---
+++ prac_03/broken_score.py
@@ -0,0 +1,23 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+
+
+def main():
+    score = float(input("Enter score: "))
+    print(determine_score_status(score))
+
+
+def determine_score_status(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score < 50:
+        return "Bad"
+    elif score < 90:
+        return "Passable"
+    else:
+        return "Excellent"
+
+
+main()

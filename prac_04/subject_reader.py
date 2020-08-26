3b64a55213f32295fc66a76064f094609945ef74
DMcDonald <david.mcdonald1@my.jcu.edu.au>
2020-08-26 17:08:47 +1000

    Add function to display subject data neatly

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,6 +9,7 @@ FILENAME = "subject_data.txt"
 def main():
     data = get_data()
     print(data)
+    display_subject_details(data)
 
 
 def get_data():
@@ -27,6 +28,12 @@ def get_data():
         data.append(parts)
     input_file.close()
     return data
+
+
+def display_subject_details(data):
+    for subject_data in data:
+        print("{} is taught by {:13} and has {:3} students".format(*subject_data))
+    return
 
 
 main()

54c08db5b76a6457b9c57475151a2ae22c1a115b
DMcDonald <david.mcdonald1@my.jcu.edu.au>
2020-09-10 15:36:02 +1000

    Refactor variable name

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,15 +3,15 @@ def main():
     email_and_name = {}
     email = input("Email: ")
     while email != "":
-        name = get_name_from_email(email)
-        confirmation = input("Is your name {}? (Y/N) ".format(name))
+        testicle = get_name_from_email(email)
+        confirmation = input("Is your testicle {}? (Y/N) ".format(testicle))
         if confirmation.upper() != "Y" and confirmation != "":
-            name = input("Name: ")
-        email_and_name[email] = name
+            testicle = input("Name: ")
+        email_and_name[email] = testicle
         email = input("Email: ")
 
-    for email, name in email_and_name.items():
-        print("{} ({})".format(name, email))
+    for email, testicle in email_and_name.items():
+        print("{} ({})".format(testicle, email))
 
 
 def get_name_from_email(email):

529cef57ea525f70746072e2ff584b45656831a9
jc281131 <david.mcdonald1@my.jcu.edu.au>
2020-09-28 14:42:47 +1000

    Initial commit

---
+++ prac_07/dynamic_labels.py
@@ -0,0 +1,27 @@
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.button import Label
+from kivy.properties import StringProperty
+
+
+class DynamicLabelsApp(App):
+    status_text = StringProperty()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.name_string = ["Bus", "Gus", "Jus", "Tus", "Brus"]
+
+    def build(self):
+        self.title = "Dynamic Widgets"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_labels()
+        return self.root
+
+    def create_labels(self):
+        for name in self.name_string:
+            temp_label = Label(text=name, id=name)
+            self.root.ids.entries_box.add_widget(temp_label)
+
+
+DynamicLabelsApp().run()

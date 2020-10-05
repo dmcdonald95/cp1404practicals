bcadd33e6c4d60f62fec807d9bb3d14e1773515d
DMcDonald <david.mcdonald1@my.jcu.edu.au>
2020-10-05 12:35:09 +1000

    Add silver_service_taxi_test program

---
+++ prac_08/silver_service_taxi_test.py
@@ -0,0 +1,12 @@
+
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    my_silver_taxi = SilverServiceTaxi("Silver taxi", 100, 2)
+    my_silver_taxi.drive(18)
+    print(my_silver_taxi)
+    print("${:.2f}".format(my_silver_taxi.get_fare()))
+
+
+main()

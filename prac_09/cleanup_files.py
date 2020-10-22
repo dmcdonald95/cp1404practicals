a5bf93576201d1155f5e0e0ee697874a908b8645
jc281131 <david.mcdonald1@my.jcu.edu.au>
2020-10-22 12:25:05 +1000

    Used up all brain cells rip

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -1,16 +1,23 @@
-"""
-CP1404/CP5632 Practical
-Demos of various os module examples
-"""
+
 import shutil
 import os
 
 
 def main():
-    """Demo os module functions."""
+    """Cleanup the song filenames."""
     print("Starting directory is: {}".format(os.getcwd()))
 
-    os.chdir('Lyrics/Christmas')
+    os.chdir('Lyrics')
+    for directory_name, subdirectories, filenames in os.walk('.'):
+        print("Directory:", directory_name)
+        print("\tcontains subdirectories:", subdirectories)
+        print("\tand files:", filenames)
+        print("(Current working directory is: {})".format(os.getcwd()))
+
+        for filename in filenames:
+            full_name = os.path.join(directory_name, filename)
+            new_name = os.path.join(directory_name, get_fixed_filename(filename))
+            os.rename(full_name, new_name)
 
     print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
 
@@ -32,22 +39,8 @@ def main():
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
     new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    new_name.enumerate(filename).islower().isupper() + "_"
     return new_name
-
-
-def demo_walk():
-    """Process all subdirectories using os.walk()."""
-    os.chdir('Lyrics')
-    for directory_name, subdirectories, filenames in os.walk('.'):
-        print("Directory:", directory_name)
-        print("\tcontains subdirectories:", subdirectories)
-        print("\tand files:", filenames)
-        print("(Current working directory is: {})".format(os.getcwd()))
-
-        for filename in filenames:
-            full_name = os.path.join(directory_name, filename)
-            new_name = os.path.join(directory_name, get_fixed_filename(filename))
-            os.rename(full_name, new_name)
 
 
 main()

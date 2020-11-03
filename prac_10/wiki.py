b6853e81bb6cadbfb33d1c725a24999961cf1712
jc281131 <david.mcdonald1@my.jcu.edu.au>
2020-11-03 12:51:49 +1000

    Add wiki api program

---
+++ prac_10/wiki.py
@@ -0,0 +1,7 @@
+
+search_phrase = input("Search: ")
+while search_phrase != "":
+    print("{0} is {0} and is very {0} so it is just like {0} where it {0}. Like many other {0} it can be very {0}"
+      "so in order to {0} we have to {0} just exactly like the {0}.".format(search_phrase))
+    search_phrase = input("Search: ")
+# Bruh it wont let me install it on lab computers, rip :(
